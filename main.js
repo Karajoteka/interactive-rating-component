382e972fc8baa0651151b93d45dc19a30ad8d333
Agu Orcha <aguorcha@gmail.com>
2023-03-21 16:49:01 +0100

    Proyecto terminado con todas las funcionalides JS

--- main.js
+++ main.js
@@ -1,30 +1,47 @@
-// Hacer que cambie la pantalla
-const submitButton = document.querySelector('.submit-button');
-const submitContainer = document.querySelector('.submit-container');
-const resultContainer = document.querySelector('.result-container');
+document.addEventListener("DOMContentLoaded", function(event) {
+  const submitButton = document.querySelector('.submit-button');
+  const submitContainer = document.querySelector('.submit-container');
+  const resultContainer = document.querySelector('.result-container');
+  const selectedNumber = document.querySelector('.selected-number');
 
-submitButton.addEventListener('click', showResultContainer);
+  const btn1 = document.querySelector('#number-1');
+  const btn2 = document.querySelector('#number-2');
+  const btn3 = document.querySelector('#number-3');
+  const btn4 = document.querySelector('#number-4');
+  const btn5 = document.querySelector('#number-5');
 
-function showResultContainer() {
-  resultContainer.classList.remove('inactive');
-  submitContainer.classList.add('inactive');
-}
+  submitButton.addEventListener('click', showResultContainer);
 
-// Hacer que aparezca, en la segunda pantalla, el número seleccionado en la primera
-const btn1 = document.querySelector('#number-1');
-const btn2 = document.querySelector('#number-2');
-const btn3 = document.querySelector('#number-3');
-const btn4 = document.querySelector('#number-4');
-const btn5 = document.querySelector('#number-5');
+  function showResultContainer() {
+    resultContainer.classList.remove('inactive');
+    submitContainer.classList.add('inactive');
 
-const selectedNumber = document.querySelector('.selectedNumber');
+    const selectedElement = document.querySelector('.number-container.selected').textContent;
+    const selectedValue = selectedElement ? selectedElement.textContent : "";
 
-function btnOnClick() {
-  console.log('Escuchando click');
-  btn1.value = 1;
-  btn2.value = 2;
-  btn3.value = 3;
-  btn4.value = 4;
-  btn5.value = 5;
-  selectedNumber.innerHTML = btn1.value;
-}
+    selectedNumber.innerHTML = selectedValue;
+  }
+
+  btn1.addEventListener('click', function() {
+    selectedNumber.innerHTML = btn1.textContent;
+  });
+
+  btn2.addEventListener('click', function() {
+    selectedNumber.innerHTML = btn2.textContent;
+  });
+
+  btn3.addEventListener('click', function() {
+    selectedNumber.innerHTML = btn3.textContent;
+  });
+
+  btn4.addEventListener('click', function() {
+    selectedNumber.innerHTML = btn4.textContent;
+  });
+
+  btn5.addEventListener('click', function() {
+    selectedNumber.innerHTML = btn5.textContent;
+  });
+});
+
+
+
